48c24ff0fc210216f15cf6a3d7401de43c1f0647
ghostsnstuff <jaredhalpert90@gmail.com>
2014-04-13 10:34:36 -0700

    Update test.js

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ var toBinary = require('./toBinary')
 var test = require('tape')
 
 test('toBinary tests', function(t) {
-	t.plan(2)
-	t.deepEqual(toBinary(98), '01100010')
-	var ch = 'k'
-	var ascii = ch.charCodeAt(0) 
-	t.deepEqual(toBinary(ascii), '01101011')
+  t.plan(2)
+  t.deepEqual(toBinary(98), '01100010')
+  var ch = 'k'
+  var ascii = ch.charCodeAt(0) 
+  t.deepEqual(toBinary(ascii), '01101011')
 })
